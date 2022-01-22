dba037e786abb615798a5ce65ddeda6ff9f5aa28
MilkyCode13 <shmayhel.andrey@gmail.com>
2022-01-22 22:01:48 +0300

    Changed enum serialization type to string

--- CloudBruh.Trustartup.FeedContent/Program.cs
+++ CloudBruh.Trustartup.FeedContent/Program.cs
@@ -1,3 +1,4 @@
+using System.Text.Json.Serialization;
 using CloudBruh.Trustartup.FeedContent.Models;
 using Microsoft.EntityFrameworkCore;
 
@@ -5,7 +6,8 @@ WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(options => options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
 builder.Services.AddDbContext<FeedContentContext>(options => options.UseInMemoryDatabase("FeedContent"));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
